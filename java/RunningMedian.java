0789eb665482c7bf9e66999ce13c996d0b9716a7
Yulong Tan <yulongtan67@gmail.com>
2019-09-29 15:08:52 -0700

    Finish running median

--- java/RunningMedian.java
+++ java/RunningMedian.java
@@ -1,3 +1,5 @@
+import java.util.PriorityQueue;
+
 /**
  * Daily Coding Problem: Problem #33 [Easy]
  * 
@@ -16,7 +18,43 @@
  */
 
 public class Solution {
+  private PriorityQueue maxHeap = new PriorityQueue<Integer>();
+  private PriorityQueue minHeap = new PriorityQueue<Integer>();
+
+  /**
+   * Runtime: O(n log (n)) - adding to the Heap is log(n).
+   * Space: O(n)
+   * @param nums
+   */
   public void runningMedian(int[] nums) {
-    
+    for (int n : nums) {
+      addNumber(n);
+      double median = getMedian();
+      System.out.println(median);
+    }
+  }
+
+  /**
+   * Add an element into minHeap first, and then remove the min
+   * and move it into the max. This ensures that all elements
+   * in the minHeap are larger than the maxHeap
+   * @param n
+   */
+  private void addNumber(int n) {
+    minHeap.add(n);
+    maxHeap.add(minHeap.poll());
+    if (minHeap.size() < maxHeap.size()) {
+      minHeap.add(maxHeap.poll());
+    }
+  }
+
+  private double getMedian() {
+    if (minHeap.size() > maxHeap.size()) {
+      return minHeap.peek();
+    } else {
+      int mid1 = minHeap.peek();
+      int mid2 = maxHeap.peek();
+      return (mid1 + mid2) / 2.0;
+    }
   }
 }
